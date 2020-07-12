de4673677f1c2fcc9395fe8d6923243d822209e6
Jacky Fong <jacky.fong@afp.com>
2020-07-12 21:52:22 +0200

    Add Sidebar logo

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="sidebar" />
+  <div class="sidebar">
+    <nuxt-link
+      :to="getUrl($i18n.locale)"
+      class="logo"
+    >
+      <img
+        src="../assets/img/logo.png"
+        alt="Logo"
+      >
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Sidebar'
+  name: 'Sidebar',
+
+  methods: {
+    getUrl (locale) {
+      return locale !== 'fr' ? `/${locale}/` : '/'
+    }
+  }
 }
 </script>
 
@@ -19,8 +35,24 @@ export default {
   left: 0;
   background: $main_color;
 
+  text-align: center;
+
   @media all and (max-width: $breakpoint-sm) {
     display: none;
+  }
+
+  & > .logo {
+    display: block;
+    width: 100%;
+    padding: 45px 0;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    & > img {
+      width: 16px;
+    }
   }
 }
 </style>
--- components/projects/NextBar.vue
+++ components/projects/NextBar.vue
@@ -62,7 +62,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "~@/assets/style/variables";
 $horizontalPadding: 115px;
 
